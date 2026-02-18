13c308b686a5a639708e2e9d4b8be5e2d767b061
litnimax <litnimaxster@gmail.com>
2026-02-18 10:01:15 +0100

    feat: sync main partner phone from first phone number by sequence

--- phone_multiple_contacts/models/partner_phone.py
+++ phone_multiple_contacts/models/partner_phone.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class PartnerPhone(models.Model):
@@ -21,3 +21,29 @@ class PartnerPhone(models.Model):
     )
     phone = fields.Char(required=True)
     note = fields.Char()
+
+    def _sync_partner_phone(self):
+        for partner in self.mapped('partner_id'):
+            first = partner.phone_ids[:1]
+            partner.phone = first.phone if first else False
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        records = super().create(vals_list)
+        records._sync_partner_phone()
+        return records
+
+    def write(self, vals):
+        res = super().write(vals)
+        if 'phone' in vals or 'sequence' in vals:
+            self._sync_partner_phone()
+        return res
+
+    def unlink(self):
+        partners = self.mapped('partner_id')
+        res = super().unlink()
+        remaining = self.env['partner.phone'].search([('partner_id', 'in', partners.ids)], order='sequence, id', limit=1)
+        for partner in partners:
+            first = remaining.filtered(lambda p: p.partner_id == partner)[:1]
+            partner.phone = first.phone if first else False
+        return res
