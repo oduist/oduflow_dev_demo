65e8d658dcf16d460feb5ab487ea9919f8e03533
litnimax <litnimaxster@gmail.com>
2026-06-14 02:30:20 +0300

    fix: bump phone_multiple_contacts version to 19.0

--- phone_multiple_contacts/__manifest__.py
+++ phone_multiple_contacts/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Multiple Phone Contacts',
-    'version': '18.0.1.0.0',
+    'version': '19.0.1.0.0',
     'category': 'Contacts',
     'summary': 'Allow multiple phone numbers per partner',
     'description': """
